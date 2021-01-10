edfc32124ce4b241a9221b8467b333bd3def24bd
Dperaltat <peraltadaniel069@gmail.com>
2021-01-09 23:13:28 -0500

    LogIn para probar

--- src/ejb/UsuarioEJB.java
+++ src/ejb/UsuarioEJB.java
@@ -3,6 +3,10 @@ package ejb;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 import entidad.Usuario;
 
@@ -14,6 +18,21 @@ public class UsuarioEJB extends AbstractFacade<Usuario>{
 	public UsuarioEJB() {
 		super(Usuario.class);
 		// TODO Auto-generated constructor stub
+	}
+	
+	public synchronized Usuario logIn(String correo, String password){
+        CriteriaBuilder criteriaBuilder= entityManager.getCriteriaBuilder();
+        CriteriaQuery<Usuario> criteriaQuery= criteriaBuilder.createQuery(Usuario.class);
+        Root<Usuario> usuarioRoot= criteriaQuery.from(Usuario.class);
+        Predicate predicate= criteriaBuilder.equal(usuarioRoot.get("correo"),correo);
+        Predicate predicate1= criteriaBuilder.equal(usuarioRoot.get("password"),password);
+        Predicate validaciones= criteriaBuilder.and(predicate,predicate1);
+        criteriaQuery.select(usuarioRoot).where(validaciones);
+        try {
+            return entityManager.createQuery(criteriaQuery).getSingleResult();
+        }catch (Exception e){
+            return null;
+        }
 	}
 
 	@Override
